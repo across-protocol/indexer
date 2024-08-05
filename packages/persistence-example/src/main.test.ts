400c8cabf5af763d5d0a6232e0df0560910c3085
james-a-morris <jaamorris@cs.stonybrook.edu>
2024-08-05 14:44:11 -0400

    feat(docker): add docker-compose file for improved dx
    Signed-off-by: james-a-morris <jaamorris@cs.stonybrook.edu>

---
+++ packages/persistence-example/src/main.test.ts
@@ -0,0 +1,9 @@
+import { expect } from "chai";
+
+import * as main from "./main";
+
+describe("main", () => {
+  it("should return true", async () => {
+    expect(true).to.be.true; // We can'd directly do much here because this is an example that connects to external services
+  });
+});
