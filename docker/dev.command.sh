c8b61d11bfefc08582521a99aca9513943068b23
james-a-morris <jaamorris@cs.stonybrook.edu>
2024-09-03 17:47:05 -0400

    chore: update compose file
    Signed-off-by: james-a-morris <jaamorris@cs.stonybrook.edu>

--- docker/dev.command.sh
+++ docker/dev.command.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-# Only run migrations if the RUN_MIGRATIONS environment variable is set to true
-if [ "$RUN_MIGRATIONS" = "true" ]; then
-    pnpm db:indexer-database:migrate:run
-fi
 pnpm --filter @repo/node-app start
