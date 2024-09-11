d0ebda26bc8c0b2b6e0871def14c28f627b32611
david <david@umaproject.org>
2024-09-11 09:22:41 -0400

    simplify rangequerystore
    Signed-off-by: david <david@umaproject.org>

--- packages/indexer/src/rangeQueryStore.ts
+++ packages/indexer/src/rangeQueryStore.ts
@@ -40,64 +40,6 @@ export class RangeQueryStore {
     return [fromBlock, toBlock].join("~");
   }
   /**
-   * Creates a new block range entry in Redis.
-   * @param {number} fromBlock - The starting block of the range.
-   * @param {number} toBlock - The ending block of the range.
-   * @throws Will throw an error if the range already exists.
-   * @returns {Promise<RangeQuery>} The created range query.
-   */
-  async create(fromBlock: number, toBlock: number) {
-    const id = this.id(fromBlock, toBlock);
-    assert(!(await this.has(id)), "Range already exists");
-    return this.set(id);
-  }
-  /**
-   * Adds a range entry to Redis by its identifier.
-   * @param {string} id - The identifier of the range.
-   * @returns {Promise<RangeQuery>} The range query associated with the id.
-   */
-  async set(id: string): Promise<RangeQuery> {
-    await this.config.redis.sadd(this.config.prefix, id);
-    return this.decode(id);
-  }
-  /**
-   * Retrieves a block range from Redis by its identifier.
-   * @param {string} id - The identifier of the range.
-   * @throws Will throw an error if the range does not exist.
-   * @returns {Promise<RangeQuery>} The range query associated with the id.
-   */
-  async get(id: string) {
-    assert(await this.has(id), `Range not found with id ${id}`);
-    return this.decode(id);
-  }
-
-  /**
-   * Checks if a block range exists in Redis by its identifier.
-   * @param {string} id - The identifier of the range.
-   * @returns {Promise<boolean>} True if the range exists, false otherwise.
-   */
-  async has(id: string) {
-    const count = await this.config.redis.sismember(this.config.prefix, id);
-    return count === 1;
-  }
-  /**
-   * Removes a block range from Redis by its identifier.
-   * @param {string} id - The identifier of the range to delete.
-   * @returns {Promise<void>}
-   */
-  async del(id: string) {
-    await this.config.redis.srem(this.config.prefix, id);
-  }
-  /**
-   * Adds a range entry to Redis by specifying the block range.
-   * @param {number} fromBlock - The starting block of the range.
-   * @param {number} toBlock - The ending block of the range.
-   * @returns {Promise<RangeQuery>} The range query associated with the id.
-   */
-  async setByRange(fromBlock: number, toBlock: number) {
-    return this.set(this.id(fromBlock, toBlock));
-  }
-  /**
    * Decodes a range identifier into a RangeQuery object.
    * @private
    * @param {string} id - The identifier of the range.
@@ -115,6 +57,24 @@ export class RangeQueryStore {
       fromBlock: Number(fromBlock),
       toBlock: Number(toBlock),
     };
+  }
+  /**
+   * Adds a range entry to Redis by its identifier.
+   * @param {string} id - The identifier of the range.
+   * @returns {Promise<RangeQuery>} The range query associated with the id.
+   */
+  async set(id: string): Promise<RangeQuery> {
+    await this.config.redis.sadd(this.config.prefix, id);
+    return this.decode(id);
+  }
+  /**
+   * Adds a range entry to Redis by specifying the block range.
+   * @param {number} fromBlock - The starting block of the range.
+   * @param {number} toBlock - The ending block of the range.
+   * @returns {Promise<RangeQuery>} The range query associated with the id.
+   */
+  async setByRange(fromBlock: number, toBlock: number) {
+    return this.set(this.id(fromBlock, toBlock));
   }
   /**
    * Retrieves all block range entries from Redis.
