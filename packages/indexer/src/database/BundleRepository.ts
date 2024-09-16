afd4e4fd1c631ada5dace7e9b2435b7b22f41299
james-a-morris <jaamorris@cs.stonybrook.edu>
2024-09-16 16:52:39 -0400

    chore: use bundle repository
    Signed-off-by: james-a-morris <jaamorris@cs.stonybrook.edu>

---
+++ packages/indexer/src/database/BundleRepository.ts
@@ -0,0 +1,204 @@
+import winston from "winston";
+import * as across from "@across-protocol/sdk";
+import { DataSource, entities, utils } from "@repo/indexer-database";
+
+/**
+ * An abstraction class for interacting with the database for bundle-related operations.
+ */
+export class BundleRepository extends utils.BaseRepository {
+  constructor(
+    postgres: DataSource,
+    logger: winston.Logger,
+    throwError: boolean,
+  ) {
+    super(postgres, logger, throwError);
+  }
+
+  /**
+   * Retrieves all canceled root bundle events that haven't been associated to a bundle relation yet.
+   * @returns A list of partial canceled root bundle entities
+   */
+  public retrieveUnassociatedCanceledEvents(): Promise<
+    Pick<
+      entities.RootBundleCanceled,
+      "blockNumber" | "transactionHash" | "logIndex" | "transactionIndex" | "id"
+    >[]
+  > {
+    const canceledRootBundleRepository = this.postgres.getRepository(
+      entities.RootBundleCanceled,
+    );
+    // Find all canceled events that haven't been associated with a bundle.
+    return canceledRootBundleRepository
+      .createQueryBuilder("drb")
+      .select([
+        "drb.id",
+        "drb.blockNumber",
+        "drb.logIndex",
+        "drb.transactionIndex",
+      ])
+      .leftJoin("bundle", "b", "b.cancelationId = drb.id")
+      .where("b.cancelationId IS NULL")
+      .getMany();
+  }
+
+  /**
+   * Retrieves all disputes that haven't been associated to a bundle relation yet.
+   * @returns A list of partial disputed root bundle entities
+   */
+  public retrieveUnassociatedDisputedEvents(): Promise<
+    Pick<
+      entities.RootBundleDisputed,
+      "blockNumber" | "transactionHash" | "logIndex" | "transactionIndex" | "id"
+    >[]
+  > {
+    const disputedRootBundleRepository = this.postgres.getRepository(
+      entities.RootBundleDisputed,
+    );
+
+    // Find all disputed events that haven't been associated with a bundle.
+    return disputedRootBundleRepository
+      .createQueryBuilder("drb")
+      .select([
+        "drb.id",
+        "drb.blockNumber",
+        "drb.logIndex",
+        "drb.transactionIndex",
+      ])
+      .leftJoin("bundle", "b", "b.disputeId = drb.id")
+      .where("b.disputeId IS NULL")
+      .getMany();
+  }
+
+  public retrieveUnassociatedProposedRootBundleEvents(): Promise<
+    Pick<
+      entities.ProposedRootBundle,
+      "slowRelayRoot" | "poolRebalanceRoot" | "relayerRefundRoot" | "id"
+    >[]
+  > {
+    const proposedRootBundleRepository = this.postgres.getRepository(
+      entities.ProposedRootBundle,
+    ); // Grab all relevant proposed root bundle events that haven't been associated yet with a bundle.
+    // This query uses a LEFT JOIN to find `ProposedRootBundle` records (`prb`) where no `Bundle`
+    // is associated via the `proposal` foreign key (indicated by checking for NULL values in the `Bundle` table).
+    return proposedRootBundleRepository
+      .createQueryBuilder("prb")
+      .select([
+        "prb.id",
+        "prb.poolRebalanceRoot",
+        "prb.relayerRefundRoot",
+        "prb.slowRelayRoot",
+      ])
+      .leftJoin("bundle", "b", "b.proposalId = prb.id")
+      .where("b.proposalId IS NULL")
+      .getMany();
+  }
+
+  /**
+   * Retrieves the closest proposed root bundle to the given block number, transaction index, and log index. The
+   * proposed root bundle can be no further back than the given max lookback from the provided block number.
+   * @param blockNumber The block number to search from
+   * @param transactionIndex The transaction index in the block to search from
+   * @param logIndex The log index in the transaction to search from
+   * @param maxLookbackFromBlock The maximum number of blocks to look back from the provided block number
+   * @returns The closest proposed root bundle back in time, or undefined if none are found
+   */
+  public retrieveClosestProposedRootBundle(
+    blockNumber: number,
+    transactionIndex: number,
+    logIndex: number,
+    maxLookbackFromBlock: number,
+  ): Promise<Pick<entities.ProposedRootBundle, "id"> | null> {
+    const proposedRootBundleRepository = this.postgres.getRepository(
+      entities.ProposedRootBundle,
+    );
+    return proposedRootBundleRepository
+      .createQueryBuilder("prb")
+      .select(["prb.id"])
+      .where(
+        // Proposal is in the past
+        "(prb.blockNumber < :blockNumber OR " +
+          // Proposal happened earlier in the block
+          "(prb.blockNumber = :blockNumber AND prb.transactionIndex < :transactionIndex) OR " +
+          // Proposal happened earlier in the same transaction
+          "(prb.blockNumber = :blockNumber AND prb.transactionIndex = :transactionIndex AND prb.logIndex < :logIndex)) AND " +
+          // Ensure the block difference is less than an average bundle length in ETH blocks
+          "prb.blockNumber > :blockDiff",
+        {
+          blockNumber,
+          transactionIndex,
+          logIndex,
+          blockDiff: blockNumber - maxLookbackFromBlock,
+        },
+      )
+      .orderBy("prb.blockNumber", "DESC") // Grab the most recent proposal
+      .getOne();
+  }
+
+  /**
+   * Creates a bundle for each proposed root bundle event that hasn't been associated with a bundle yet.
+   * @param proposalEvents Unassociated proposed root bundle events that need to be associated with a bundle
+   * @returns The number of bundles created
+   */
+  public async createBundlesForProposedEvents(
+    proposalEvents: Pick<
+      entities.ProposedRootBundle,
+      "slowRelayRoot" | "poolRebalanceRoot" | "relayerRefundRoot" | "id"
+    >[],
+  ): Promise<number> {
+    const bundleRepository = this.postgres.getRepository(entities.Bundle);
+    const results = await bundleRepository.insert(
+      proposalEvents.map((event) =>
+        bundleRepository.create({
+          poolRebalanceRoot: event.poolRebalanceRoot,
+          relayerRefundRoot: event.relayerRefundRoot,
+          slowRelayRoot: event.slowRelayRoot,
+          proposalId: event.id,
+          status: entities.BundleStatus.Proposed, // Default to proposed status
+        }),
+      ),
+    );
+    return results.identifiers.length;
+  }
+
+  public async associateEventsToBundle(
+    events: (
+      | {
+          bundleId: number;
+          eventId: number;
+        }
+      | undefined
+    )[],
+    eventType: "canceled" | "disputed",
+  ): Promise<number> {
+    const dbValues = {
+      canceled: {
+        dbKey: "cancelationId",
+        dbStatus: entities.BundleStatus.Canceled,
+      },
+      disputed: {
+        dbKey: "disputeId",
+        dbStatus: entities.BundleStatus.Disputed,
+      },
+    };
+    const { dbKey, dbStatus } = dbValues[eventType];
+    const bundleRepository = this.postgres.getRepository(entities.Bundle);
+
+    const results = await Promise.all(
+      events.map((event) => {
+        if (!event) {
+          return undefined;
+        }
+        return bundleRepository.update(
+          {
+            id: event.bundleId,
+          },
+          {
+            [dbKey]: { id: event.eventId },
+            status: dbStatus,
+          },
+        );
+      }),
+    );
+    return results.filter((x) => x).length;
+  }
+}
--- packages/indexer/src/services/bundles.ts
+++ packages/indexer/src/services/bundles.ts
@@ -1,6 +1,7 @@
 import { DataSource, entities } from "@repo/indexer-database";
 import Redis from "ioredis";
 import winston from "winston";
+import { BundleRepository } from "../database/BundleRepository";
 
 const AVERAGE_BUNDLE_LIVENESS_SECONDS = 60 * 60; // 1 hour
 const AVERAGE_SECONDS_PER_BLOCK = 13; // 13 seconds per block on ETH
@@ -10,7 +11,8 @@ const AVERAGE_BLOCKS_PER_BUNDLE = Math.floor(
 
 /**
  * Error thrown when the processor configuration is malformed
- */ class ConfigurationMalformedError extends Error {
+ */
+class ConfigurationMalformedError extends Error {
   constructor() {
     super("Processor configuration is malformed");
     this.name = "ProcessorConfigurationMalformedError";
@@ -39,10 +41,12 @@ export function Processor(config: BundleConfig) {
     throw new ConfigurationMalformedError();
   }
 
+  const bundleRepository = new BundleRepository(postgres, logger, true);
+
   return async () => {
-    await assignBundleToProposedEvent(postgres, logger);
-    await assignDisputeEventToBundle(postgres, logger);
-    await assignCanceledEventToBundle(postgres, logger);
+    await assignBundleToProposedEvent(bundleRepository, logger);
+    await assignDisputeEventToBundle(bundleRepository, logger);
+    await assignCanceledEventToBundle(bundleRepository, logger);
   };
 }
 
@@ -70,104 +74,46 @@ function logResultOfAssignment(
 }
 
 /**
- * Retrieves the closest proposed root bundle to the given block number, transaction index, and log index. The
- * proposed root bundle can be no further back than the given max lookback from the provided block number.
- * @param blockNumber The block number to search from
- * @param transactionIndex The transaction index in the block to search from
- * @param logIndex The log index in the transaction to search from
- * @param maxLookbackFromBlock The maximum number of blocks to look back from the provided block number
- * @param dataSource The data source to query from
- * @returns The closest proposed root bundle back in time, or undefined if none are found
- */
-function retrieveClosestProposedRootBundle(
-  blockNumber: number,
-  transactionIndex: number,
-  logIndex: number,
-  maxLookbackFromBlock: number,
-  dataSource: DataSource,
-): Promise<entities.ProposedRootBundle | null> {
-  const proposedRootBundleRepository = dataSource.getRepository(
-    entities.ProposedRootBundle,
-  );
-  return proposedRootBundleRepository
-    .createQueryBuilder("prb")
-    .select(["prb.id"])
-    .where(
-      // Proposal is in the past
-      "(prb.blockNumber < :blockNumber OR " +
-        // Proposal happened earlier in the block
-        "(prb.blockNumber = :blockNumber AND prb.transactionIndex < :transactionIndex) OR " +
-        // Proposal happened earlier in the same transaction
-        "(prb.blockNumber = :blockNumber AND prb.transactionIndex = :transactionIndex AND prb.logIndex < :logIndex)) AND " +
-        // Ensure the block difference is less than an average bundle length in ETH blocks
-        "prb.blockNumber > :blockDiff",
-      {
-        blockNumber,
-        transactionIndex,
-        logIndex,
-        blockDiff: blockNumber - maxLookbackFromBlock,
-      },
-    )
-    .orderBy("prb.blockNumber", "DESC") // Grab the most recent proposal
-    .getOne();
-}
-
-/**
  * Assigns disputed events to bundle entities if they haven't been associated yet.
  * @param dataSource A valid connection to the database
  * @param logger A logger instance
  * @returns A void promise
  */
 async function assignDisputeEventToBundle(
-  dataSource: DataSource,
+  dbRepository: BundleRepository,
   logger: winston.Logger,
 ): Promise<void> {
-  const bundleRepository = dataSource.getRepository(entities.Bundle);
-  const disputedRootBundleRepository = dataSource.getRepository(
-    entities.RootBundleDisputed,
-  );
-
-  // Find all disputed events that haven't been associated with a bundle.
-  const disputedEventsWithoutBundleAssociated =
-    await disputedRootBundleRepository
-      .createQueryBuilder("drb")
-      .select(["drb.id", "drb.blockNumber", "drb.logIndex"])
-      .leftJoin("bundle", "b", "b.disputeId = drb.id")
-      .where("b.disputeId IS NULL")
-      .getMany();
-
-  const updatedEvents = await Promise.all(
-    disputedEventsWithoutBundleAssociated.map(
+  const unassignedDisputedEvents =
+    await dbRepository.retrieveUnassociatedDisputedEvents();
+  const eventAssociations = await Promise.all(
+    unassignedDisputedEvents.map(
       async ({ blockNumber, id, logIndex, transactionIndex }) => {
-        const proposedBundle = await retrieveClosestProposedRootBundle(
-          blockNumber,
-          transactionIndex,
-          logIndex,
-          AVERAGE_BLOCKS_PER_BUNDLE,
-          dataSource,
-        );
+        const proposedBundle =
+          await dbRepository.retrieveClosestProposedRootBundle(
+            blockNumber,
+            transactionIndex,
+            logIndex,
+            AVERAGE_BLOCKS_PER_BUNDLE,
+          );
         if (!proposedBundle) {
           return undefined;
         }
-        return bundleRepository.update(
-          {
-            proposal: { id: proposedBundle.id },
-          },
-          {
-            dispute: { id },
-            status: entities.BundleStatus.Disputed,
-          },
-        );
+        return {
+          bundleId: proposedBundle.id,
+          eventId: id,
+        };
       },
     ),
   );
-  const numberUpdated = updatedEvents.filter((x) => x).length;
-
+  const updatedEventCount = await dbRepository.associateEventsToBundle(
+    eventAssociations,
+    "disputed",
+  );
   logResultOfAssignment(
     logger,
     "RootBundleDisputed",
-    disputedEventsWithoutBundleAssociated.length,
-    numberUpdated,
+    unassignedDisputedEvents.length,
+    updatedEventCount,
   );
 }
 
@@ -177,53 +123,39 @@ async function assignDisputeEventToBundle(
  * @param logger A logger instance
  */
 async function assignCanceledEventToBundle(
-  dataSource: DataSource,
+  dbRepository: BundleRepository,
   logger: winston.Logger,
 ): Promise<void> {
-  const bundleRepository = dataSource.getRepository(entities.Bundle);
-  const canceledRootBundleRepository = dataSource.getRepository(
-    entities.RootBundleCanceled,
-  );
-  // Find all canceled events that haven't been associated with a bundle.
-  const canceledEventsWithoutBundleAssociated =
-    await canceledRootBundleRepository
-      .createQueryBuilder("drb")
-      .select(["drb.id", "drb.blockNumber", "drb.logIndex"])
-      .leftJoin("bundle", "b", "b.cancelationId = drb.id")
-      .where("b.cancelationId IS NULL")
-      .getMany();
-
-  const updatedEvents = await Promise.all(
-    canceledEventsWithoutBundleAssociated.map(
+  const unassignedCanceledEvents =
+    await dbRepository.retrieveUnassociatedCanceledEvents();
+  const eventAssociations = await Promise.all(
+    unassignedCanceledEvents.map(
       async ({ blockNumber, id, logIndex, transactionIndex }) => {
-        const proposedBundle = await retrieveClosestProposedRootBundle(
-          blockNumber,
-          transactionIndex,
-          logIndex,
-          AVERAGE_BLOCKS_PER_BUNDLE,
-          dataSource,
-        );
+        const proposedBundle =
+          await dbRepository.retrieveClosestProposedRootBundle(
+            blockNumber,
+            transactionIndex,
+            logIndex,
+            AVERAGE_BLOCKS_PER_BUNDLE,
+          );
         if (!proposedBundle) {
           return undefined;
         }
-        return bundleRepository.update(
-          {
-            proposal: { id: proposedBundle.id },
-          },
-          {
-            cancelation: { id },
-            status: entities.BundleStatus.Canceled,
-          },
-        );
+        return {
+          bundleId: proposedBundle.id,
+          eventId: id,
+        };
       },
     ),
   );
-  const numberUpdated = updatedEvents.filter((x) => x).length;
-
+  const numberUpdated = await dbRepository.associateEventsToBundle(
+    eventAssociations,
+    "canceled",
+  );
   logResultOfAssignment(
     logger,
     "RootBundleCanceled",
-    canceledEventsWithoutBundleAssociated.length,
+    unassignedCanceledEvents.length,
     numberUpdated,
   );
 }
@@ -236,48 +168,19 @@ async function assignCanceledEventToBundle(
  * @returns A void promise
  */
 async function assignBundleToProposedEvent(
-  dataSource: DataSource,
+  dbRepository: BundleRepository,
   logger: winston.Logger,
 ): Promise<void> {
-  const proposedRootBundleRepository = dataSource.getRepository(
-    entities.ProposedRootBundle,
+  const unassignedProposedEvents =
+    await dbRepository.retrieveUnassociatedProposedRootBundleEvents();
+  const createdBundleCount = await dbRepository.createBundlesForProposedEvents(
+    unassignedProposedEvents,
   );
-  const bundleRepository = dataSource.getRepository(entities.Bundle);
-
-  // Grab all relevant proposed root bundle events that haven't been associated yet with a bundle.
-  // This query uses a LEFT JOIN to find `ProposedRootBundle` records (`prb`) where no `Bundle`
-  // is associated via the `proposal` foreign key (indicated by checking for NULL values in the `Bundle` table).
-  const proposedEventsWithoutBundleAssociated =
-    await proposedRootBundleRepository
-      .createQueryBuilder("prb")
-      .select([
-        "prb.id",
-        "prb.poolRebalanceRoot",
-        "prb.relayerRefundRoot",
-        "prb.slowRelayRoot",
-      ])
-      .leftJoin("bundle", "b", "b.proposalId = prb.id")
-      .where("b.proposalId IS NULL")
-      .getMany();
-
-  // Perform a bulk insert of the new bundles.
-  const resultingInsert = await bundleRepository.insert(
-    proposedEventsWithoutBundleAssociated.map((event) =>
-      bundleRepository.create({
-        poolRebalanceRoot: event.poolRebalanceRoot,
-        relayerRefundRoot: event.relayerRefundRoot,
-        slowRelayRoot: event.slowRelayRoot,
-        proposal: event,
-        status: entities.BundleStatus.Proposed, // Default to proposed status
-      }),
-    ),
-  );
-
   // Log the results of the operation.
   logResultOfAssignment(
     logger,
     "ProposedRootBundle",
-    proposedEventsWithoutBundleAssociated.length,
-    resultingInsert.identifiers.length,
+    unassignedProposedEvents.length,
+    createdBundleCount,
   );
 }
