f8fd1a641830656e2b36e748db56e715854dbb86
Alexandru Matei <alexandrumatei3693@gmail.com>
2024-09-27 13:49:30 +0300

    Fix

--- packages/indexer/src/data-indexing/service/Indexer.ts
+++ packages/indexer/src/data-indexing/service/Indexer.ts
@@ -30,47 +30,73 @@ export class Indexer {
   async start() {
     while (true) {
       try {
-        const lastBlockFinalised = await this.redisCache.get<number>(
-          this.getLastFinalisedBlockCacheKey(),
-        );
-        const fromBlock = lastBlockFinalised
-          ? lastBlockFinalised + 1
-          : this.dataHandler.getStartIndexingBlockNumber();
-        const latestBlockNumber = await this.rpcProvider.getBlockNumber();
-        // TODO: hardcoded 200_000, should be a config or removed
-        const toBlock = Math.min(fromBlock + 200_000, latestBlockNumber);
+        const { latestBlockNumber, blockRange } = await this.getBlockRange();
 
-        if (fromBlock > toBlock) {
+        if (!blockRange) {
           this.logger.info({
             at: "Indexer::start",
             message: "No new blocks to process",
             latestBlockNumber,
             dataIdentifier: this.dataHandler.getDataIdentifier(),
           });
-          await across.utils.delay(this.config.loopWaitTimeSeconds);
-          continue;
+        } else {
+          const lastFinalisedBlockInBlockRange =
+            this.getLastFinalisedBlockInBlockRange(
+              latestBlockNumber,
+              blockRange,
+            );
+          await this.dataHandler.processBlockRange(
+            blockRange,
+            lastFinalisedBlockInBlockRange,
+          );
+          await this.redisCache.set(
+            this.getLastFinalisedBlockCacheKey(),
+            lastFinalisedBlockInBlockRange,
+          );
         }
-        const blockRange: BlockRange = { from: fromBlock, to: toBlock };
-        const lastOnChainFinalisedBlock =
-          latestBlockNumber - this.config.finalisedBlockBufferDistance + 1;
-        const lastBlockRangeFinalisedBlock = Math.min(
-          toBlock,
-          lastOnChainFinalisedBlock,
-        );
-        await this.dataHandler.processBlockRange(
-          blockRange,
-          lastBlockRangeFinalisedBlock,
-        );
-        await this.redisCache.set(
-          this.getLastFinalisedBlockCacheKey(),
-          lastBlockRangeFinalisedBlock,
-        );
-        await across.utils.delay(this.config.loopWaitTimeSeconds);
       } catch (error) {
-        this.logger.error(`Indexer::Error processing block range`, error);
+        this.logger.error({
+          at: "Indexer::start",
+          message: "Error processing block range",
+          dataIdentifier: this.dataHandler.getDataIdentifier(),
+          error,
+        });
+      } finally {
         await across.utils.delay(this.config.loopWaitTimeSeconds);
       }
     }
+  }
+
+  private getLastFinalisedBlockInBlockRange(
+    latestBlockNumber: number,
+    blockRange: BlockRange,
+  ) {
+    const lastOnChainFinalisedBlock =
+      latestBlockNumber - this.config.finalisedBlockBufferDistance + 1;
+    const lastFinalisedBlockInBlockRange = Math.min(
+      blockRange.to,
+      lastOnChainFinalisedBlock,
+    );
+
+    return lastFinalisedBlockInBlockRange;
+  }
+
+  private async getBlockRange() {
+    const lastBlockFinalised = await this.redisCache.get<number>(
+      this.getLastFinalisedBlockCacheKey(),
+    );
+    const latestBlockNumber = await this.rpcProvider.getBlockNumber();
+    // If the last block finalised is the same as the latest block, no new blocks to process
+    if (latestBlockNumber === lastBlockFinalised) {
+      return { latestBlockNumber, blockRange: undefined };
+    }
+    const fromBlock = lastBlockFinalised
+      ? lastBlockFinalised + 1
+      : this.dataHandler.getStartIndexingBlockNumber();
+    // TODO: hardcoded 200_000, should be a config or removed
+    const toBlock = Math.min(fromBlock + 200_000, latestBlockNumber);
+    const blockRange: BlockRange = { from: fromBlock, to: toBlock };
+    return { latestBlockNumber, blockRange };
   }
 
   private getLastFinalisedBlockCacheKey() {
