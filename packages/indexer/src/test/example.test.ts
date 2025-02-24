d71c209f5db793a390753253a7da2f50f5e891e5
Melisa Guevara <melisaguevarav@gmail.com>
2025-02-24 14:04:37 -0300

    fix mocha env vars

--- packages/indexer/src/test/example.test.ts
+++ packages/indexer/src/test/example.test.ts
@@ -1,23 +1,34 @@
 import { expect } from "chai";
 import { parsePostgresConfig } from "../parseEnv";
-import { createDataSource, entities, fixtures } from "@repo/indexer-database";
+import {
+  createDataSource,
+  Repository,
+  entities,
+  fixtures,
+} from "@repo/indexer-database";
 
-// this is in here because mocha crashes if no tests are found
 describe("example", () => {
+  let repository: Repository<entities.V3FundsDeposited>;
+  let depositsFixture: fixtures.FundsDepositedFixture;
+
+  before(async () => {
+    const databaseConfig = parsePostgresConfig(process.env);
+    const dataSource = await createDataSource(databaseConfig).initialize();
+    repository = dataSource.getRepository(entities.V3FundsDeposited);
+    depositsFixture = new fixtures.FundsDepositedFixture(dataSource);
+  });
+
+  after(async () => {
+    await depositsFixture.deleteAllDeposits();
+  });
+
   it("should return true", async () => {
-    console.log("this is running");
-    console.log("this is running with a change in the test file");
     expect(true).to.be.true;
   });
 
+  // Example test showing database interaction
   it("should create an entry in the database", async () => {
-    console.log(process.env);
-    const databaseConfig = parsePostgresConfig(process.env);
-    const dataSource = await createDataSource(databaseConfig).initialize();
-    const repository = dataSource.getRepository(entities.V3FundsDeposited);
-    const fixture = new fixtures.FundsDepositedFixture(dataSource);
-    const mockedDeposit = fixture.mockFundsDeposited({});
-    await fixture.insertDeposits([mockedDeposit]);
+    await depositsFixture.insertDeposits([]);
     const depositRows = await repository.find();
     console.log(depositRows);
     expect(depositRows.length).to.be.equal(1);
