7708f10542e80bf7b14c5f2564e80dcd64a4ae87
amateima <amatei@umaproject.org>
2025-06-10 15:09:53 +0300

    Fix tests

--- packages/indexer-api/src/tests/deposit.test.ts
+++ packages/indexer-api/src/tests/deposit.test.ts
@@ -2,7 +2,12 @@ import { expect } from "chai";
 import request from "supertest";
 import { ExpressApp } from "../express-app";
 import express from "express";
-import { createDataSource, DataSource, fixtures } from "@repo/indexer-database";
+import {
+  createDataSource,
+  DataSource,
+  entities,
+  fixtures,
+} from "@repo/indexer-database";
 import Redis from "ioredis";
 import * as Indexer from "@repo/indexer";
 import * as utils from "../utils";
@@ -13,6 +18,7 @@ describe("/deposit", () => {
   let dataSource: DataSource;
   let redis: Redis;
   let depositsFixture: fixtures.FundsDepositedFixture;
+  let relayHashInfoFixture: fixtures.RelayHashInfoFixture;
 
   before(async () => {
     // Set up database and Redis
@@ -24,7 +30,9 @@ describe("/deposit", () => {
 
     // Initialize fixtures
     depositsFixture = new fixtures.FundsDepositedFixture(dataSource);
+    relayHashInfoFixture = new fixtures.RelayHashInfoFixture(dataSource);
     await depositsFixture.deleteAllDeposits();
+    await relayHashInfoFixture.deleteAllRelayHashInfoRows();
 
     // Initialize the Express app with the deposits router
     const depositsRouter = routers.deposits.getRouter(dataSource, redis);
@@ -41,7 +49,12 @@ describe("/deposit", () => {
   it("should return 200 and one deposit by depositId and originChainId", async () => {
     // Insert a test deposit
     const [deposit] = await depositsFixture.insertDeposits([]);
-
+    const relayHashInfoData = {
+      id: 1,
+      depositEventId: deposit.id,
+      status: entities.RelayStatus.Unfilled,
+    };
+    await relayHashInfoFixture.insertRelayHashInfos([relayHashInfoData]);
     const response = await request(app).get("/deposit").query({
       depositId: deposit.id,
       originChainId: deposit.originChainId,
