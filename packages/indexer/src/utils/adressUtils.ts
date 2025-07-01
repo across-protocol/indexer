fc46e4831d904799ff2947ca060671722e9f3878
Melisa Guevara <melisaguevarav@gmail.com>
2025-07-01 16:46:53 -0300

    feat: support new Address class from SDK (#317)

--- packages/indexer/src/utils/adressUtils.ts
+++ packages/indexer/src/utils/adressUtils.ts
@@ -1,19 +1,19 @@
 import { utils, arch } from "@across-protocol/sdk";
 
-const { EvmAddress, SvmAddress, chainIsEvm, chainIsSvm, isZeroAddress } = utils;
+const { chainIsEvm, chainIsSvm } = utils;
 
-export function formatFromBytes32ToChainFormat(
-  bytes32Address: string,
+export function formatFromAddressToChainFormat(
+  address: utils.Address,
   chainId: number,
 ) {
   if (chainIsEvm(chainId)) {
-    return EvmAddress.from(bytes32Address, "base16").toEvmAddress();
+    return address.toEvmAddress();
   } else if (chainIsSvm(chainId)) {
     // Handle special case if address is the zero address
-    if (isZeroAddress(bytes32Address)) {
+    if (address.isZeroAddress()) {
       return arch.svm.SVM_DEFAULT_ADDRESS;
     }
-    return SvmAddress.from(bytes32Address, "base16").toBase58();
+    return address.toBase58();
   } else {
     throw new Error(`Unsupported chainId: ${chainId}`);
   }
