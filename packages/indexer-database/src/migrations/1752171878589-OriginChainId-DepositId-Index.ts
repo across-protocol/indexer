0eef3c1392c61530a4daa91e2ccf62580e17a143
amateima <amatei@umaproject.org>
2025-07-10 15:31:41 -0700

    fix: fix migration

--- packages/indexer-database/src/migrations/1752171878589-OriginChainId-DepositId-Index.ts
+++ packages/indexer-database/src/migrations/1752171878589-OriginChainId-DepositId-Index.ts
@@ -7,12 +7,6 @@ export class OriginChainIdDepositIdIndex1752171878589
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `DROP INDEX "evm"."IX_filledV3Relay_depositId_originChainId"`,
-    );
-    await queryRunner.query(
-      `DROP INDEX "evm"."IX_v3FundsDeposited_depositId_originChainId"`,
-    );
-    await queryRunner.query(
       `CREATE INDEX "IX_filledV3Relay_originChainId_depositId" ON "evm"."filled_v3_relay" ("originChainId", "depositId") `,
     );
     await queryRunner.query(
@@ -26,12 +20,6 @@ export class OriginChainIdDepositIdIndex1752171878589
     );
     await queryRunner.query(
       `DROP INDEX "evm"."IX_filledV3Relay_originChainId_depositId"`,
-    );
-    await queryRunner.query(
-      `CREATE INDEX "IX_v3FundsDeposited_depositId_originChainId" ON "evm"."v3_funds_deposited" ("depositId", "originChainId") `,
-    );
-    await queryRunner.query(
-      `CREATE INDEX "IX_filledV3Relay_depositId_originChainId" ON "evm"."filled_v3_relay" ("depositId", "originChainId") `,
     );
   }
 }
