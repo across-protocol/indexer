12da14fa435ed2990e7a3054f538f2288284db9a
amateima <amatei@umaproject.org>
2025-07-16 08:11:18 +0300

    Fix

--- packages/indexer/src/redis/redisCache.ts
+++ packages/indexer/src/redis/redisCache.ts
@@ -3,11 +3,13 @@ import Redis from "ioredis";
 
 export class RedisCache implements across.interfaces.CachingMechanismInterface {
   constructor(private redis: Redis) {}
+
   async get<ObjectType>(key: string): Promise<ObjectType | null> {
     const result = await this.redis.get(key);
     if (result === null) return result;
     return JSON.parse(result);
   }
+
   set<ObjectType, _OverrideType>(
     key: string,
     value: ObjectType,
@@ -19,5 +21,17 @@ export class RedisCache implements across.interfaces.CachingMechanismInterface {
       return this.redis.set(key, JSON.stringify(value), "EX", 2147483647);
     }
     return this.redis.set(key, JSON.stringify(value));
+  }
+
+  public async pub(channel: string, message: string): Promise<number> {
+    return this.redis.publish(channel, message);
+  }
+
+  public async sub(channel: string, listener: (message: string, channel: string) => void): Promise<number> {
+    await this.redis.subscribe(channel);
+    this.redis.on('message', (channel, message) => {
+      listener(message, channel);
+    })
+    return 1;
   }
 }
