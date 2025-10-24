2fac235cbd7459c95ce3710081935fe882491ed8
Nikolas Haimerl <nikolas.haimerl@proton.me>
2025-10-24 18:19:01 +0200

    feat: add SponsoredDepositForBurn entity and migration

--- packages/indexer-database/src/entities/evm/SponsoredDepositForBurn.ts
+++ packages/indexer-database/src/entities/evm/SponsoredDepositForBurn.ts
@@ -1,80 +1,70 @@
 import {
   Column,
   Entity,
-  PrimaryGeneratedColumn,
+  PrimaryColumn,
   Index,
   CreateDateColumn,
   DeleteDateColumn,
-  Unique,
 } from "typeorm";
 
-@Entity({ schema: "evm" })
-@Unique("UK_sponsoredDepositForBurn_chain_block_tx_log", [
-  "chainId",
-  "blockNumber",
-  "transactionHash",
-  "logIndex",
-])
-@Index("IX_SponsoredDepositForBurn_chainId", ["chainId"])
-@Index("IX_SponsoredDepositForBurn_quoteNonce", ["quoteNonce"])
-@Index("IX_SponsoredDepositForBurn_originSender", ["originSender"])
-@Index("IX_SponsoredDepositForBurn_finalRecipient", ["finalRecipient"])
-@Index("IX_SponsoredDepositForBurn_blockNumber", ["blockNumber"])
-@Index("IX_SponsoredDepositForBurn_finalised", ["finalised"])
-@Index("IX_SponsoredDepositForBurn_blockTimestamp", ["blockTimestamp"])
-@Index("IX_SponsoredDepositForBurn_deletedAt", ["deletedAt"])
+@Entity()
 export class SponsoredDepositForBurn {
-  @PrimaryGeneratedColumn()
-  id: number;
+  @PrimaryColumn("varchar")
+  id!: string; // txHash:logIndex
 
-  @Column({ type: "bigint" })
-  chainId: string;
+  @Index()
+  @Column("varchar")
+  quoteNonce!: string;
 
-  @Column({ nullable: true })
-  quoteNonce: string;
+  @Index()
+  @Column("varchar")
+  originSender!: string;
 
-  @Column()
-  originSender: string;
+  @Index()
+  @Column("varchar")
+  finalRecipient!: string;
 
-  @Column()
-  finalRecipient: string;
+  @Column("varchar")
+  quoteDeadline!: string;
 
-  @Column()
-  quoteDeadline: Date;
+  @Column("varchar")
+  maxBpsToSponsor!: string;
 
-  @Column({ type: "bigint" })
-  maxBpsToSponsor: string;
+  @Column("varchar")
+  maxUserSlippageBps!: string;
 
-  @Column({ type: "bigint" })
-  maxUserSlippageBps: string;
+  @Column("varchar")
+  finalToken!: string;
 
-  @Column()
-  finalToken: string;
+  @Column("varchar")
+  signature!: string;
 
-  @Column()
-  signature: string;
+  @Index()
+  @Column("int")
+  blockNumber!: number;
 
-  @Column()
-  blockNumber: number;
+  @Column("varchar")
+  transactionHash!: string;
 
-  @Column()
-  transactionHash: string;
+  @Column("int")
+  transactionIndex!: number;
 
-  @Column()
-  transactionIndex: number;
+  @Column("int")
+  logIndex!: number;
 
-  @Column()
-  logIndex: number;
+  @Column("boolean", { default: false })
+  finalised!: boolean;
 
-  @Column("boolean")
-  finalised: boolean;
-
-  @Column()
-  blockTimestamp: Date;
+  @Column("timestamp")
+  blockTimestamp!: Date;
 
   @CreateDateColumn()
-  createdAt: Date;
+  createdAt!: Date;
 
   @DeleteDateColumn({ nullable: true })
   deletedAt?: Date;
+
+  constructor(init: Partial<SponsoredDepositForBurn>) {
+    Object.assign(this, init);
+  }
 }
