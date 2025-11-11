90e3ddd181021918f55c0732641a75dee01ff369
Nikolas Haimerl <nikolas.haimerl@proton.me>
2025-11-11 11:19:43 +0100

    feat: add decoding functions for HyperCore withdrawal hook data and message body

--- packages/indexer/src/data-indexing/tests/CCTPIndexerDataHandler.integration.test.ts
+++ packages/indexer/src/data-indexing/tests/CCTPIndexerDataHandler.integration.test.ts
@@ -1,4 +1,4 @@
-import { expect } from "chai";
+import { assert, expect } from "chai";
 import { DataSource } from "typeorm";
 import { Logger } from "winston";
 import * as across from "@across-protocol/sdk";
@@ -10,6 +10,8 @@ import { CCTPRepository } from "../../database/CctpRepository";
 import { BlockRange } from "../model";
 import { createTestRetryProvider } from "../../tests/testProvider";
 import { entities } from "../../../../indexer-database/dist/src";
+import { decodeHookData, decodeMessageBody } from "../adapter/cctp-v2/service";
+import { ethers } from "ethers";
 
 /**
  * Test suite for the CCTPIndexerDataHandler.
@@ -140,5 +142,57 @@ describe("CCTPIndexerDataHandler", () => {
     expect(savedEvent).to.exist;
     expect(savedEvent!.transactionHash).to.equal(transactionHash);
     expect(savedEvent!.blockNumber).to.equal(blockNumber);
+  }).timeout(10000);
+  it("should fetch hypercore withdraw data and be able to decode the hookData", async () => {
+    const transactionHash =
+      "0x13b9b9dfb7f8804d385db96454d094791b8ab618556fcd37fb17c4b206499871";
+    const blockNumber = 213803846;
+    setupTestForChainId(CHAIN_IDs.ARBITRUM_SEPOLIA);
+
+    const blockRange: BlockRange = {
+      from: blockNumber,
+      to: blockNumber,
+    };
+
+    // We need to stub the filterTransactionsFromAcrossFinalizer method to avoid filtering out our test transaction
+    sinon
+      .stub(handler as any, "filterTransactionsFromAcrossFinalizer")
+      .returnsArg(0);
+
+    await handler.processBlockRange(blockRange, blockNumber);
+
+    const messageReceivedRepositroy = dataSource.getRepository(
+      entities.MessageReceived,
+    );
+
+    const savedEvent = await messageReceivedRepositroy.findOneOrFail({
+      where: { transactionHash: transactionHash },
+    });
+    // Tx can be found here: https://app.hyperliquid-testnet.xyz/explorer/tx/0xb8275884570f48f7b9a1041b70e2310113007069f20267c95bf003d7160322e2
+    // And here: https://sepolia.arbiscan.io/tx/0x13b9b9dfb7f8804d385db96454d094791b8ab618556fcd37fb17c4b206499871
+    const decodedMessageBody = decodeMessageBody(savedEvent.messageBody);
+    assert(decodedMessageBody, "Expected to decode messageBody");
+    expect(decodedMessageBody.version).to.equal(1);
+    expect(decodedMessageBody.burnToken).to.equal(
+      "0x0000000000000000000000002b3370ee501b4a559b57d449569354196457d8ab",
+    );
+    expect(decodedMessageBody.mintRecipient).to.equal(
+      "0x0000000000000000000000003f51b87ae65548ab996bdbb363f2553a311ef43e",
+    );
+    expect(decodedMessageBody.amount.toNumber()).to.equal(3000000);
+    expect(decodedMessageBody.maxFee.toNumber()).to.equal(200000);
+
+    const decodedHookData = decodeHookData(decodedMessageBody.hookData);
+    assert(decodedHookData, "Expected to decode hookData");
+    expect(decodedHookData.hyperCoreNonce.toNumber()).to.equal(1762785559609);
+    expect(decodedHookData.fromAddress).to.equal(
+      "0x3f51b87ae65548ab996bdbb363f2553a311ef43e",
+    );
+    expect(
+      ethers.utils.toUtf8String(
+        ethers.utils.arrayify(decodedHookData.magicBytes),
+      ),
+    ).to.equal("cctp-forward");
+    expect(decodedHookData.versionId).to.equal(0);
   }).timeout(10000);
 });
