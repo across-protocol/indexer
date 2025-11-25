b26a320e83458434ff5ebbb3b84c931bc6a5eb53
Ashwin <213675439+ashwinrava@users.noreply.github.com>
2025-11-25 14:18:37 -0500

    fix: add missing indexes (#464)

--- packages/indexer-database/src/migrations/1764096928688-ForeignKeyIndexes.ts
+++ packages/indexer-database/src/migrations/1764096928688-ForeignKeyIndexes.ts
@@ -5,21 +5,6 @@ export class ForeignKeyIndexes1764096928688 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `CREATE INDEX "IX_rhi_depositEventId" ON "relay_hash_info" ("depositEventId") `,
-    );
-    await queryRunner.query(
-      `CREATE INDEX "IX_rhi_fillEventId" ON "relay_hash_info" ("fillEventId") `,
-    );
-    await queryRunner.query(
-      `CREATE INDEX "IX_rhi_slowFillRequestEventId" ON "relay_hash_info" ("slowFillRequestEventId") `,
-    );
-    await queryRunner.query(
-      `CREATE INDEX "IX_rhi_swapBeforeBridgeEventId" ON "relay_hash_info" ("swapBeforeBridgeEventId") `,
-    );
-    await queryRunner.query(
-      `CREATE INDEX "IX_rhi_callsFailedEventId" ON "relay_hash_info" ("callsFailedEventId") `,
-    );
-    await queryRunner.query(
       `CREATE INDEX "IX_swapMetadata_relayHashInfoId_side_deletedAt" ON "evm"."swap_metadata" ("relayHashInfoId", "side", "deletedAt") WHERE "deletedAt" IS NULL`,
     );
     await queryRunner.query(
@@ -32,14 +17,5 @@ export class ForeignKeyIndexes1764096928688 implements MigrationInterface {
     await queryRunner.query(
       `DROP INDEX "evm"."IX_swapMetadata_relayHashInfoId_side_deletedAt"`,
     );
-    await queryRunner.query(`DROP INDEX "public"."IX_rhi_callsFailedEventId"`);
-    await queryRunner.query(
-      `DROP INDEX "public"."IX_rhi_swapBeforeBridgeEventId"`,
-    );
-    await queryRunner.query(
-      `DROP INDEX "public"."IX_rhi_slowFillRequestEventId"`,
-    );
-    await queryRunner.query(`DROP INDEX "public"."IX_rhi_fillEventId"`);
-    await queryRunner.query(`DROP INDEX "public"."IX_rhi_depositEventId"`);
   }
 }
