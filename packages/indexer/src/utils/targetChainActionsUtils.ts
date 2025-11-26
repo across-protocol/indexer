f591cd433e62464e038e70b2f73321cf4794342d
Dong-Ha Kim <dongha.kim210@gmail.com>
2025-11-26 09:55:53 +0100

    chore: identify usdh transfers to hypercore (#462) (#467)
    Co-authored-by: Melisa Guevara <melisaguevarav@gmail.com>

--- packages/indexer/src/utils/targetChainActionsUtils.ts
+++ packages/indexer/src/utils/targetChainActionsUtils.ts
@@ -3,7 +3,8 @@ import { EventDecoder } from "../web3/EventDecoder";
 import { entities } from "@repo/indexer-database";
 
 export const TARGET_CHAIN_ACTION_ADDRESSES: Record<string, string> = {
-  "0x200000000000000000000000000000000000010C": "1337", // HyperCore
+  "0x200000000000000000000000000000000000010C": "1337", // HyperCore USDT0 System Address
+  "0x2000000000000000000000000000000000000168": "1337", // HyperCore USDH System Address
 };
 
 export interface FillTargetChainActionPair {
