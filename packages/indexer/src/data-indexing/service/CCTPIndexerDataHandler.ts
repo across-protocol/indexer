b8f864ae069dabf30ea935337b5081bac7916996
amateima <89395931+amateima@users.noreply.github.com>
2025-12-08 23:41:59 +0200

    feat: production release 🚀  (#507)
    Co-authored-by: Ashwin <213675439+ashwinrava@users.noreply.github.com>

--- packages/indexer/src/data-indexing/service/CCTPIndexerDataHandler.ts
+++ packages/indexer/src/data-indexing/service/CCTPIndexerDataHandler.ts
@@ -128,7 +128,6 @@ const WHITELISTED_FINALIZERS = [
   "0x9A8f92a830A5cB89a3816e3D267CB7791c16b04D",
   "0x72adB07A487f38321b6665c02D289C413610B081",
   "0x49066b9c4a68e0942f77989e78d9e27f78a67ce7b165cafd101a477a148058fd",
-  "0xb63c02e60C05F05975653edC83F876C334E07C6d", // CCTPHyperEVMSponsoredDstHandler
 ];
 
 // Convert whitelisted finalizers to bytes32 format for comparison with destinationCaller
