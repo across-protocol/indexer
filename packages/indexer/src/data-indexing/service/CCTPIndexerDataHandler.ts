57a4badf278c94570161d954e5e9f74fa437f6f1
amateima <amatei@umaproject.org>
2025-12-09 17:43:23 +0200

    chore: update sponsored contracts addresses

--- packages/indexer/src/data-indexing/service/CCTPIndexerDataHandler.ts
+++ packages/indexer/src/data-indexing/service/CCTPIndexerDataHandler.ts
@@ -111,7 +111,7 @@ const MESSAGE_TRANSMITTER_ADDRESS_TESTNET: string =
 // TODO: Update this address once the contract is deployed
 const SPONSORED_CCTP_DST_PERIPHERY_ADDRESS: { [key: number]: string } = {
   // Taken from https://hyperevmscan.io/address/0x7B164050BBC8e7ef3253e7db0D74b713Ba3F1c95#code
-  [CHAIN_IDs.HYPEREVM]: "0x7B164050BBC8e7ef3253e7db0D74b713Ba3F1c95",
+  [CHAIN_IDs.HYPEREVM]: "0xb63c02e60C05F05975653edC83F876C334E07C6d",
 };
 
 // TODO: Update this address once the contract is deployed
@@ -128,6 +128,7 @@ const WHITELISTED_FINALIZERS = [
   "0x9A8f92a830A5cB89a3816e3D267CB7791c16b04D",
   "0x72adB07A487f38321b6665c02D289C413610B081",
   "0x49066b9c4a68e0942f77989e78d9e27f78a67ce7b165cafd101a477a148058fd",
+  "0xb63c02e60C05F05975653edC83F876C334E07C6d", // CCTPHyperEVMSponsoredDstHandler
 ];
 
 // Convert whitelisted finalizers to bytes32 format for comparison with destinationCaller
