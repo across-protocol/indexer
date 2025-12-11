d651c5fa5254910c152be47c491ed5fceeebfad8
ashwinrava <213675439+ashwinrava@users.noreply.github.com>
2025-12-11 12:05:00 -0500

    Address comments

--- packages/indexer-api/src/services/deposits.ts
+++ packages/indexer-api/src/services/deposits.ts
@@ -485,7 +485,7 @@ export class DepositsService {
             statusResponse.status === "pending"
               ? entities.RelayStatus.Unfilled
               : entities.RelayStatus.Filled;
-          fillTx = statusResponse.fillTx || fillTx;
+          fillTx = statusResponse.fillTx ?? undefined;
         }
         // For OFT deposits, use the status function
         else if (
@@ -512,7 +512,7 @@ export class DepositsService {
             statusResponse.status === "pending"
               ? entities.RelayStatus.Unfilled
               : entities.RelayStatus.Filled;
-          fillTx = statusResponse.fillTx || fillTx;
+          fillTx = statusResponse.fillTx ?? undefined;
         }
         // For Across deposits, use existing logic
         else {
