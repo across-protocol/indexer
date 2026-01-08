da5d3a222caddb29617d047f7b30e494ea9d7f37
Alexander Liteplo <alexanderliteplo@gmail.com>
2026-01-08 13:02:21 -0300

    remove v3 funds deposited block index

--- packages/indexer-database/src/entities/evm/V3FundsDeposited.ts
+++ packages/indexer-database/src/entities/evm/V3FundsDeposited.ts
@@ -36,10 +36,6 @@ import { RelayHashInfo } from "../RelayHashInfo";
   "originChainId",
   "depositId",
 ])
-@Index("IX_v3FundsDeposited_originChainId_blockNumber", [
-  "originChainId",
-  "blockNumber",
-])
 @Index("IX_v3FundsDeposited_internalHash", ["internalHash"])
 export class V3FundsDeposited {
   @PrimaryGeneratedColumn()
